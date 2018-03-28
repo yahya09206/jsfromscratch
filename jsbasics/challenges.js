79f03f827f2f853aedd29be01b63abb87940d1a7
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-27 20:12:37 -0700

    finished with challenge

--- jsbasics/challenges.js
+++ jsbasics/challenges.js
@@ -4,3 +4,18 @@
 /*
 CHALLENGE 1
 */
+var johnHeight = 147;
+var friendHeight = 157;
+var johnAge = 25;
+var friendAge = 30;
+
+var johnScore = johnHeight + 5 * johnAge;
+var friendScore = friendHeight + 5 * friendAge;
+
+if (johnScore > friendScore) {
+	console.log('John wins the game! with a score of: ' + johnScore);
+}else if(friendScore > johnScore){
+	console.log('Friend wins the game! with a score of: ' + friendScore);
+}else{
+	console.log('Game ends in a draw!');
+}
