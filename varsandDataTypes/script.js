e7bf12bf6021a5483687df99d7df91787365d747
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-27 18:18:22 -0700

    variables

---
+++ varsandDataTypes/script.js
@@ -0,0 +1,8 @@
+//variables and data types
+//variable is like a container in which we can store a valuable that can be used over and over again
+
+//declare variable
+var name = 'john'; //john is a string data type because it is a word
+
+//write to console
+console.log(name); //use the name of the variable inside the parentheses 
