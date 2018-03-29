45313dca48e67d4e4f9ee69207d7f17128117f23
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-28 19:36:04 -0700

    finished wiht first part of challnege

--- jsbasics/challenges.js
+++ jsbasics/challenges.js
@@ -20,3 +20,17 @@ if (johnScore > friendScore) {
 }
 
 /*CHALLENGE 2 */
+var years = [1990, 2000, 2005, 1991];
+var ages = [];
+
+for(i = 0; i < years.length; i++){
+	ages[i] = 2017 - years[i];
+}
+
+for(var i = 0; i < ages.length; i++){
+	if(ages[i] >= 18){
+		console.log('Person is ' + ages[i] + ' years old, and is of full age');
+	}else{
+		console.log('Person is ' + ages[i] + ' years old, and is not of full age');
+	}
+}
