4eeecb297944a636f1088a771901db38333be912
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-28 19:42:35 -0700

    finished with 2nd part of challenge

--- jsbasics/challenges.js
+++ jsbasics/challenges.js
@@ -34,3 +34,24 @@ for(var i = 0; i < ages.length; i++){
 		console.log('Person is ' + ages[i] + ' years old, and is not of full age');
 	}
 }
+
+function printFullAge(years){
+	var ages = [];
+	var fullAges = [];
+	for(i = 0; i < years.length; i++){
+		ages[i] = 2017 - years[i];
+	}
+
+	for(var i = 0; i < ages.length; i++){
+		if(ages[i] >= 18){
+			console.log('Person is ' + ages[i] + ' years old, and is of full age');
+			fullAges.push(true);
+		}else{
+			console.log('Person is ' + ages[i] + ' years old, and is not of full age');
+			fullAges.push(false);
+		}
+	}
+	return fullAges;
+}
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2012, 1915,1995]);
