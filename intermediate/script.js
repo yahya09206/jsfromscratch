d7b94aa666b491bb850691c77bfd878951ea9300
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-28 20:59:10 -0700

    finished with intermediate

--- intermediate/script.js
+++ intermediate/script.js
@@ -64,6 +64,12 @@ var john = {
 	yearOfBirth: 1990,
 	calculateAge: function(){
 		console.log(this);
+		console.log(2018 - this.yearOfBirth);
+
+		function innerFunction(){
+			console.log(this);
+		}
+		innerFunction();
 	}
 }
 john.calculateAge();
