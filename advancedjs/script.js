0a51dc2c68fff93ef96d1d5889196d8e7baafe03
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-30 19:27:08 -0700

    iife

--- advancedjs/script.js
+++ advancedjs/script.js
@@ -123,6 +123,42 @@ console.log(rates);
 console.log(fullAges);
 console.log(ages);
 
+/* ---------------------------------------------- 
+			FUNCTIONS RETURNING FUNCTIONS
+---------------------------------------------- */
+function interviewQuestion(job){
+	if(job === 'designer'){
+		return function(name){
+			console.log(name + ', can you please explain what UX design is?');
+		}
+	}else if(job === 'teacher'){
+		return function(name){
+			console.log('What subject do you teach, ' + name + '?');
+		}
+	}else{
+		return function(name){
+			console.log('Hello ' + name + ', what do you do?');
+		}
+	}
+}
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
+
+/* ---------------------------------------------- 
+			IIFE
+---------------------------------------------- */
+/*
+*/
+function game(){
+	var score = Math.random() * 10;
+	console.log(score >= 5);
+}
+game();
+ (function(){
+ 	var score = Math.random() * 19;
+ 	console.log(score >= 5);
+ })();
+
 
 
 
