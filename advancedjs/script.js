0743e1ce0cff9291a4c1a14fb41dad102d70393c
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-30 17:45:16 -0700

    done with notes, now time to build objects

---
+++ advancedjs/script.js
@@ -0,0 +1,18 @@
+/* ---------------------------------------------- 
+					OBJECTS
+---------------------------------------------- */
+/*
+- Everything is an object
+	• Arrays, Functions, Objects, Dates, Wrappers for numbers, strings and booleans.
+- Not just for storing data
+- OOP
+	• Objects interact with one another through methods and properties
+	• Used to store data, structure applications into modules and keeping code clean
+- Object constructor is the blueprint and is used to create instances to create something from the object
+- Inheritance
+	• When one object gets access to another object's properties
+	• is made possible through prototyping
+		• each and every object in javascript has a prototype property
+- The constructors prototype property is not the prototype of the constructor itself, its the prototype 
+	of all instances that are created through it
+*/
